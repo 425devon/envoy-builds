6af2870111770801516a408d9571b6f4f8e1e71e
Devon Langendoerfer <devon.langendoerfer@konghq.com>
2022-06-28 14:46:45 -0700

    sync

--- tools/envoy/build_linux.sh
+++ tools/envoy/build_linux.sh
@@ -39,7 +39,6 @@ echo "SOURCE_DIR=${SOURCE_DIR}"
 echo "--add-host=${REMOTE_CACHE_SEVER_HOSTNAME}:${REMOTE_CACHE_SEVER_IP}"
 
 docker build --add-host="${REMOTE_CACHE_SEVER_HOSTNAME}:${REMOTE_CACHE_SEVER_IP}" -t "${LOCAL_BUILD_IMAGE}" --progress=plain \
-  --build-arg REMOTE_CACHE_SEVER="${REMOTE_CACHE_SEVER_HOSTNAME}" \
   --build-arg ENVOY_BUILD_IMAGE="${ENVOY_BUILD_IMAGE}" \
   --build-arg BUILD_CMD="${BUILD_CMD}" \
   -f "${WORK_DIR:-.}/tools/envoy/Dockerfile.build-ubuntu" "${SOURCE_DIR}"
